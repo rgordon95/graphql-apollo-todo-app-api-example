ee63da9aac5d58fe438b25f2c8d0de71e48994be
Richard Gordon <richgordon95@gmail.com>
2020-10-10 23:19:08 -0400

    config apollo server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const cors = require('cors');
 const dotEnv = require('dotenv');
 
@@ -15,12 +15,28 @@ app.use(cors());
 
 app.use(express.json());
 
+const typeDefs = gql`
+    type Query {
+        greetings: String
+    }
+`;
+
+const resolvers = {};
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+apolloServer.applyMiddleware({app, path: '/graphql'});
+
 const PORT = process.env.PORT || 3000;
 
 app.use('/', (req, res, next) => {
-    res.send({ message: 'heasdallo' })
+    res.send({ message: 'hello' })
 })
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`)
+    console.log(`GRAPHQL ENDPOINT: ${apolloServer.graphqlPath}`)
 });
